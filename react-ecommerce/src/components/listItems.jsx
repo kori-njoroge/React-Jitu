26585161adbad4d976fe8db62112a179ca18919f
Kori Njoroge <korinjoroge63@gmail.com>
2023-02-06 16:04:09 +0300

    products card styling

--- react-ecommerce/src/components/listItems.jsx
+++ react-ecommerce/src/components/listItems.jsx
@@ -3,9 +3,10 @@ import React from 'react'
 export default function ListItems({ item }) {
     return (
         <div className="itemCard" key={item.id}>
-            <p>{item.title}</p>
+            <p className='itemTitle'>{item.title}</p>
             <img className="topdealsImg" src={item.image} alt={item.title} />
             <br />
+            <h4 className='price'>${item.price}</h4>
             <button>View Details</button>
         </div>
     )
